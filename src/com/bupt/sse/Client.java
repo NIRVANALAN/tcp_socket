af915bddcacc96e4dc279a638a5ee104650bc766
maver <maverickers@outlook.com>
2018-12-10 18:58:06 +0800

    add sum calculation in server, need more modifications in client. void finalize() is added to destroy vars in finally{}

--- src/com/bupt/sse/Client.java
+++ src/com/bupt/sse/Client.java
@@ -10,6 +10,12 @@ public class Client {
     public int port = 1122;
     public String host = "localhost";
     public final int length = 100;
+    OutputStream outputStream = null;
+    PrintWriter printWriter = null;
+    InputStream inputStream = null;
+    //        InputStreamReader inputStreamReader = null;
+    BufferedReader bufferedReader = null;
+    Socket socket = null;
 
     public void single_thread() throws InterruptedException, IOException {
         Socket[] sockets = new Socket[length];
@@ -24,18 +30,12 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Client client = new Client();
+    public void client_wellworked() throws IOException {
 //        client.single_thread();
-        OutputStream outputStream = null;
-        PrintWriter printWriter = null;
-        InputStream inputStream = null;
-//        InputStreamReader inputStreamReader = null;
-        BufferedReader bufferedReader = null;
-        Socket socket = null;
+
         try {
             // init and bind
-            socket = new Socket(client.host, client.port);
+            socket = new Socket(host, port);
             // get outputStream
             outputStream = socket.getOutputStream(); // byte output steam
             // pack outputStream to print stream
@@ -57,21 +57,65 @@ public class Client {
                 System.out.printf("Response from server: " + server_info_string);
             }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void client_calculate() throws IOException {
+        int a = 1, b = 2;
+        try {
+            // init and bind
+            socket = new Socket(host, port);
+            // get outputStream
+            outputStream = socket.getOutputStream(); // byte output steam
+            // pack outputStream to print stream
+            printWriter = new PrintWriter(outputStream);
+            // write to outputStream
+            // start calculation
+            printWriter.write(a);
+            printWriter.write(b);
+            // flush
+            printWriter.flush();
+            // close outputStream
+//            outputStream.close();
+            socket.shutdownOutput();
+
+            // get inputStream and read response info
+            inputStream = socket.getInputStream();
+            // use BufferRead
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String server_info_string = null;
+            while ((server_info_string = bufferedReader.readLine()) != null) {
+                System.out.print("Response from server: " + server_info_string);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // close
-            if (bufferedReader != null)
-                bufferedReader.close();
-            if (inputStream != null)
-                inputStream.close();
-            if (printWriter != null)
-                printWriter.close();
-            if (outputStream != null)
-                outputStream.close();
-            if (socket != null)
-                socket.close();
         }
+
+    }
+
+    // detor
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        // close
+        if (bufferedReader != null)
+            bufferedReader.close();
+        if (inputStream != null)
+            inputStream.close();
+        if (printWriter != null)
+            printWriter.close();
+        if (outputStream != null)
+            outputStream.close();
+        if (socket != null)
+            socket.close();
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
     }
 }
--- src/com/bupt/sse/ServerThread.java
+++ src/com/bupt/sse/ServerThread.java
@@ -5,6 +5,9 @@ import java.net.Socket;
 
 public class ServerThread extends Thread {
     Socket socket = null;
+    int[] Fibonacci = new int[2];
+    boolean login = false;
+    int index = 0;
 
     public ServerThread(Socket socket) {
         this.socket = socket;
@@ -23,17 +26,28 @@ public class ServerThread extends Thread {
             inputStream = socket.getInputStream();
             inputStreamReader = new InputStreamReader(inputStream);
             bufferedReader = new BufferedReader(inputStreamReader);
-            String client_info_string = null;
-            while ((client_info_string = bufferedReader.readLine()) != null) {
-                System.out.println("Receive info from client: " + client_info_string);
-            }
-            // close
-            socket.shutdownInput();
             // get outputStream to client
             outputStream = socket.getOutputStream();
             printWriter = new PrintWriter(outputStream);
-            // response the client
-            printWriter.write("Request received. You are registered");
+            String client_info_string = null;
+            while ((client_info_string = bufferedReader.readLine()) != null) {
+                if (login == false && client_info_string.equals("username:2016211930;password:123456")) {
+                    login = true;
+                    System.out.println("Receive login request from client: " + client_info_string);
+                } else {
+                    // calculate sum
+                    Fibonacci[index] = Integer.parseInt(client_info_string);
+                    index = 1 - index;
+                    if (index == 0) {
+                        // string to int
+                        printWriter.write(String.valueOf(Fibonacci[0] + Fibonacci[1]));
+                    }
+                }
+            }
+            // close
+            socket.shutdownInput();
+            // response the client when the first connection is confirmed
+            printWriter.write("Request received. You are Logged in. Calculation will start next");
             // flush
             printWriter.flush();
 
